9c7b3b92e8d43e97d316b4d10cc34835182fce70
nikhila koneru <nikiko1002@gmail.com>
2025-07-24 12:39:19 -0400

    restapi with dynamoDB and S3 integration

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -1,6 +1,5 @@
-#!/bin/bash 
-echo "Running tests..." 
-docker compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from test 
-exit_code=$? 
-docker compose -f docker-compose.test.yml down exit 
-$exit_code
+#!/bin/bash
+set -e
+echo "Running tests..."
+docker compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from test
+docker compose -f docker-compose.test.yml down
